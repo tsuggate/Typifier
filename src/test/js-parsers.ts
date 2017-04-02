35c8354209baa43ef626ab07746681b1e7035bea
Toby Suggate <sugto555@gmail.com>
2017-04-03 10:45:36 +1200

    Parse define block.

--- src/test/js-parsers.ts
+++ src/test/js-parsers.ts
@@ -1,15 +1,34 @@
 import {parse} from "../transpiler/parser-lib/parsers-m";
-import {libsArray} from "../transpiler/js-parsers";
-
+import {define, funcAnon, libsArray} from "../transpiler/js-parsers";
 
 
 export function jsParsers() {
+
   describe('libsArray', () => {
     it(`['jquery', 'underscore']`, () => {
-      const res = parse(libsArray, `["jquery", "underscore"]`);
+      const res = parse(libsArray, `['jquery', 'underscore']`);
 
       console.log('res: ', res);
     });
   });
+
+  describe('funcAnon', () => {
+    const code = 'function($, b, c) { }';
+
+    it(code, () => {
+      const res = parse(funcAnon, code);
+      console.log(res);
+    });
+  });
+
+  describe('define', () => {
+    const code = `define(['jquery'], function($) {});`;
+
+    it(code, () => {
+      const res = parse(define, code);
+      console.log(res);
+    });
+  });
+
 }
 
--- src/transpiler/js-parsers.ts
+++ src/transpiler/js-parsers.ts
@@ -1,9 +1,14 @@
-import {parse, word, and, repSep, stringLiteral} from "./parser-lib/parsers-m";
+import {__, and, ident, or, regex, repSep, stringLiteral} from "./parser-lib/parsers-m";
 
 
-export const define = word('define');
+export const jsString = or(regex(/'([^'\\]|\\.)*'/), stringLiteral);
 
-export const libsArray = and('[', repSep(stringLiteral, ','), ']');
+export const libsArray = and('[', repSep(jsString, ','), ']');
+
+export const funcArgs = repSep(ident, ',');
+
+export const funcAnon = and('function', __, '(', __, funcArgs, __, ')', __, '{', __,  '}');
+
+export const define = and('define', __, '(', __, libsArray, __, ',', __, funcAnon, __, ')');
 
 
-// parse()
