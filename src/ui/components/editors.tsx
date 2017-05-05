ac7d03429ae04b3da3f1f8f60f70cd3ed2012306
Toby Suggate <t.suggate@adinstruments.com>
2017-05-05 16:23:26 +1200

    Show file names above editors.

--- src/ui/components/editors.tsx
+++ src/ui/components/editors.tsx
@@ -1,5 +1,6 @@
 import * as React from 'React';
 import AceEditor from 'react-ace';
+import * as _ from 'underscore';
 
 import './editors.less';
 
@@ -18,29 +19,47 @@ export class Editors extends React.Component<EditorsProps, {}> {
    render() {
       return (
          <div className="Editors">
-            <AceEditor
-               mode="javascript"
-               theme="github"
-               name="editor1"
-               width="50%"
-               height="100%"
-               value={this.props.javascriptCode}
-               readOnly={true}
-               onChange={() => {}}
-               editorProps={{$blockScrolling: 1}}
-            />
-            <AceEditor
-               mode="javascript"
-               theme="github"
-               width="50%"
-               height="100%"
-               name="editor2"
-               value={this.props.typescriptCode}
-               readOnly={true}
-               onChange={() => {}}
-               editorProps={{$blockScrolling: 1}}
-            />
+            <div className="leftEditor">
+               <div className="leftTitle">{this.getJavascriptFileName()}</div>
+               <AceEditor
+                  mode="javascript"
+                  theme="github"
+                  name="editor1"
+                  width="100%"
+                  height="100%"
+                  value={this.props.javascriptCode}
+                  readOnly={true}
+                  onChange={() => {}}
+                  editorProps={{$blockScrolling: 1}}
+               />
+            </div>
+            <div className="rightEditor">
+               <div className="rightTitle">{this.getTypeScriptFileName()}</div>
+               <AceEditor
+                  mode="javascript"
+                  theme="github"
+                  width="100%"
+                  height="100%"
+                  name="editor2"
+                  value={this.props.typescriptCode}
+                  readOnly={true}
+                  onChange={() => {}}
+                  editorProps={{$blockScrolling: 1}}
+               />
+            </div>
          </div>
       );
+   }
+
+   getJavascriptFileName = () => {
+      const nameParts = this.props.javascriptFile.split('\\');
+
+      return _.last(nameParts, 3).join('\\');
+   };
+
+   getTypeScriptFileName = () => {
+      const name = this.getJavascriptFileName().split('.')[0];
+
+      return name + '.ts';
    }
 }
