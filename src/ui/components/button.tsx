3f0d92c4efaa98db4a4598e0013b3108d6b5438c
Toby Suggate <t.suggate@adinstruments.com>
2017-05-08 13:39:09 +1200

    Tidy up.

--- src/ui/components/button.tsx
+++ src/ui/components/button.tsx
@@ -1,21 +1,14 @@
 import * as React from 'react';
 import './button.less';
 
-export type ButtonColour = 'blue' | 'transparent';
 
 interface ButtonProps {
-   // colour: ButtonColour;
    onClick: React.MouseEventHandler<HTMLButtonElement>;
    children?: any;
    disabled?: boolean;
 }
 
 export default function Button(props: ButtonProps) {
-   // const className = [
-   //    'Button',
-   //    getColourClass(props.colour)
-   // ].join(' ');
-
    return (
       <button className="Button" onClick={props.onClick} disabled={props.disabled} >
          {props.children}
@@ -23,15 +16,3 @@ export default function Button(props: ButtonProps) {
    );
 }
 
-
-// function getColourClass(colour: ButtonColour) {
-//    switch (colour) {
-//       case 'blue':
-//          return 'Button-blue';
-//       case 'transparent':
-//          return 'Button-transparent';
-//
-//       default:
-//          throw new Error(`Invalid colour name: ${colour}`);
-//    }
-// }
